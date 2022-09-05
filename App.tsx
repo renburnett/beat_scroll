3721fa3af3c988407c59d5ff787905c1e3e09c1a
Ren Burnett <13320440+renburnett@users.noreply.github.com>
2022-09-05 14:47:32 -0700

    basic timer working

--- App.tsx
+++ App.tsx
@@ -1,9 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#03a9f4',
+    accent: '#1de9b6',
+  },
+};
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -14,8 +24,10 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
+        <PaperProvider theme={theme}>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </PaperProvider>
       </SafeAreaProvider>
     );
   }
